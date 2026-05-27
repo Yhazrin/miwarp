0e94f72f5037f495eac5cf14c4988c9939be02c0
Yhazrin <thomas41yhz@gmail.com>
2026-05-27 18:16:10 +0800

    i18n: localize TaskExecutionMonitor waiting message
    Add taskExecution_waiting key and replace hardcoded string.

--- src/lib/components/TaskExecutionMonitor.svelte
+++ src/lib/components/TaskExecutionMonitor.svelte
@@ -215,7 +215,7 @@
         >
           <path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-        <p>Waiting for execution logs...</p>
+        <p>{t('taskExecution_waiting')}</p>
       </div>
     {:else}
       <div class="space-y-1">
